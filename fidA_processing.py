85f751a39395930e03de99b9fc3b525b3ea84371
stonesunturned <colleen.em.bailey@gmail.com>
2025-08-07 22:35:54 -0400

    Update fidA_processing.py

--- fidA_processing.py
+++ fidA_processing.py
@@ -181,7 +181,7 @@ def op_addrcvrs(indat,phasept=0,mode='w',coilcombos=None):
         # Won't avfids have different size depending on the size of fids??
         if coilcombos is None:
             # Jamie unwraps the phase in the Matlab version but only ever uses the angle. Maybe for viewing? Leaving it wrapped for now
-            phs=[np.angle(avfids[phasept,nct,0,0])*180/np.pi for nct in range(avfids.shape[1])]
+            phs=[np.angle(avfids[phasept,nct,0])*180/np.pi for nct in range(avfids.shape[1])]
             if mode=='w':
                 sigs=np.abs(avfids[phasept,:,0])
             elif mode=='h':
